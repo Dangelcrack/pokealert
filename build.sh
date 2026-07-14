31f4b9bfd6a97bfee127d76f778790f91b90ec34
Dangelcrack <angelguerrero540@gmail.com>
2026-07-14 19:35:54 +0200

    fix: compilar tailwind directo en style.css para asegurar produccion

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# 1. Instalar dependencias de Python
 pip install -r requirements.txt
 
+# 2. Instalar Node y compilar Tailwind DIRECTO en el style.css
 npm install
-npx tailwindcss -i ./static/css/style.css -o ./static/css/style.min.css --minify
+npx tailwindcss -i ./static/css/style.css -o ./static/css/style.css --minify
 
+# 3. Preparar estáticos de Django y migraciones
 python manage.py collectstatic --no-input
 python manage.py migrate
